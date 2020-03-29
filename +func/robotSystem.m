31afa54f987bee9c4faabbbba9777c2dc500172b
unknown <origane.y.aa@m.titech.ac.jp>
2020-03-29 18:05:11 +0900

    modified output noise

--- +func/robotSystem.m
+++ +func/robotSystem.m
@@ -1,6 +1,6 @@
 function q_next = robotSystem(q, u, wheel, delta_t)
     Kv = [1000 0;0 990];     % motor constant value
-    wheel_speed = Kv * u;         % dual wheel speed [v_right, v_left]
+    wheel_speed = Kv * u + 10*rand(2,1);         % dual wheel speed [v_right, v_left]
     L = vecnorm( wheel, 2, 2);
     theta_dot = wheel_speed(1,1)/L(1,1) - wheel_speed(2,1)/L(2,1);
     v = [1/2 1/2]*wheel_speed;
