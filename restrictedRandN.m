2f9b186664779c4239d9205c420d376419499f84
unknown <origane.y.aa@m.titech.ac.jp>
2020-03-29 15:41:09 +0900

    add noise

---
+++ restrictedRandN.m
@@ -0,0 +1,12 @@
+% 範囲制限付き正規分布を返す
+% @oaram nu : 平均値
+% @param sigma：標準偏差
+% @range 範囲を-range ~ rangeに制限
+function y = restrictedRandN(nu, sigma, range)
+    while 1
+        y = sigma*randn + nu;
+        if abs(y-nu)<=range
+            break;
+        end
+    end
+end
