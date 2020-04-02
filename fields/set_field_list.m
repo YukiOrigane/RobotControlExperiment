20b01d5833990636f08c6bdf3f57a2c583b16a4e
unknown <origane.y.aa@m.titech.ac.jp>
2020-04-02 18:15:47 +0900

    add fields

--- fields/set_field_list.m
+++ fields/set_field_list.m
@@ -3,3 +3,6 @@ field_list('01') = "01_straight_line";
 field_list('02') = "02_line_and_circle";
 field_list('03') = '03_wall_test';
 field_list('04') = '04_straight_line_stop';
+field_list('05') = '05_straight_line_wall';
+field_list('06') = '06_hook_shaped';
+field_list('07') = '07_ssr';
