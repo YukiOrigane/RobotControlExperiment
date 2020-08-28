372d1fe08194062ba313a52e5ebbb2a12371a317
unknown <origane.y.aa@m.titech.ac.jp>
2020-08-28 19:07:50 +0900

    modified to select enable initial position noise

--- list_system_config.m
+++ list_system_config.m
@@ -1,14 +1,17 @@
 system_config('time_constant') = "off"; % 1次遅れ系（慣性由来）の時定数 0で適用せず
 system_config('wheel_noise') = "off";   % ホイール出力に偏ったノイズを入れるか否か
 system_config('add_wheel') = "off"; % 独立二輪以外を使用するか否か
+system_config('initial_position_noise') = "off"; % 初期位置誤差を使用するか否か
 
 if exist('system_lebel','var')
     switch system_lebel
         case 0  % 全部オフ
         case 1
             system_config('time_constant') = "on";
+            system_config('initial_position_noise') = "on";
         case 2
             system_config('time_constant') = "on";
             system_config('wheel_noise') = "on";
+            system_config('initial_position_noise') = "on";
     end
 end
