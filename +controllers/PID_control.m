99ebddfe94403adeba479118ca12fc4f54e71443
Yuki Onishi <yuki.st16@gmail.com>
2020-08-28 16:55:56 +0900

    Normalize feedback values
    in order to use same gains with both P and Bangbang control

--- +controllers/PID_control.m
+++ +controllers/PID_control.m
@@ -7,19 +7,18 @@
 % gain[4] : 回転成分ゲイン Kd
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function u = PID_control(t, delta_t, value_light_sensor, value_range_sensor, gain)
-    persistent sum_e pre_e
-    
-    z = sum(value_light_sensor,'all')/5;
-    e = 160-z;
-    
-    if isempty(sum_e)
-        sum_e = 0;
-        pre_e = e;
-    end
+persistent sum_e pre_e
 
-    sum_e = sum_e + e*delta_t;
-    u = [1 1 1 1;1 -1 -1 -1]*diag(gain)*[1; e; sum_e; (e-pre_e)/delta_t];   % 変換行列 * ゲイン対角行列 * 量
+z = sum(value_light_sensor,'all')/5;
+e = (160-z) / 160;
+
+if isempty(sum_e)
+    sum_e = 0;
     pre_e = e;
-    
+end
+sum_e = sum_e + e*delta_t;
+u = [1 1 1 1;1 -1 -1 -1]*diag(gain)*[1; e; sum_e; (e-pre_e)/delta_t];   % 変換行列 * ゲイン対角行列 * 量
+pre_e = e;
+
 %disp(z)
 end
